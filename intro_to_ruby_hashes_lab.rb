4e039bb0164a9445b9299adbdda266503477a73a
Tissiana Alves <altissiana@gmail.com>
2019-10-31 23:59:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,9 @@ def monopoly_with_third_tier
       :pieces => 4,
       :rent_in_dollars => {
         :one_piece_owned => 25,
-        :two => nil,
-        :three => nil,
-        :four => nil
+        :two_pieces_owned => 50,
+        :three_pieces_owned => nil,
+        :four_pieces_owned => nil
       },
       :names => {
         :one => nil,
